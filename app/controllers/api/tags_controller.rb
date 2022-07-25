3bba2f0272435b2910547e843ae606b4406303e0
zack alsiday <alsidayzack@gmail.com>
2022-07-25 12:30:33 -0700

    Tags backend completed

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,43 @@
+class Api::TagsController < ApplicationController
+    def index
+        @tags = Tag.all
+        render :index
+    end
+
+    def show
+        @tag = Tag.find_by(id: params[:id])
+        render :show
+    end
+
+    def create
+        @tag = Tag.new(tag_params)
+        
+        if @tag.save
+            render :show
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @tag = Tag.find_by(id: params[:id])
+        
+        if @tag
+          newOne = @tag.update_attributes(tag_params)
+            render :show
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @tag = Tag.find_by(id: params[:id])
+        @tag.destroy 
+        render :show 
+    end
+
+    private
+    def tag_params
+        params.require(:tag).permit(:name, :user_id)
+    end
+end
---
+++ app/views/api/tags/index.json.jbuilder
@@ -0,0 +1,7 @@
+
+
+@tags.each do |tag|
+    json.set! tag.id do
+        json.partial! 'tag', tag: tag
+    end
+end
