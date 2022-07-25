e5f73e8e7adacbd45c075f1f8955c6b657fb0afe
zack alsiday <alsidayzack@gmail.com>
2022-07-25 12:17:07 -0700

    continuing on noteTags

--- app/models/note.rb
+++ app/models/note.rb
@@ -10,6 +10,15 @@ class Note < ApplicationRecord
     belongs_to :notebook,
     foreign_key: :notebook_id,
     class_name: "Notebook"
+
+    has_many :note_tags,
+    foreign_key: :note_id,
+    class_name: "NoteTag",
+    dependent: :destroy
+    
+    has_many :tags,
+        through: :note_tags,
+        source: :tag
     
     
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,4 +5,13 @@ class Tag < ApplicationRecord
         foreign_key: :user_id,
         class_name: "User"
 
+            
+    has_many :note_tags,
+        foreign_key: :tag_id,
+        class_name: "NoteTag"
+
+    has_many :notes,
+        through: :note_tags,
+        source: :note
+
 end
