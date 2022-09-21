e6c34fe8c360aa067755e4adcfef91b11a7a917d
zack alsiday <alsidayzack@gmail.com>
2022-09-21 14:05:57 -0700

    fixing reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 import usersReducer from "./users_reducer";
 import NotesReducer from "./notes_reducer";
-import notebooksReducer from "./notebooks_Reducer";
+import notebooksReducer from "./notebooks_reducer";
 import tagsReducer from "./tags_reducer"
 import noteTagsReducer from "./note_tags_reducer";
 const entitiesReducer = combineReducers({
